00630d86afd5300dc624ef20182efa37d3830993
Sarthak Srivastava <145271363+imsrthk19@users.noreply.github.com>
2026-01-22 23:03:08 +0530

    Module for capitalizing strings, reversing strings, and counting vowels

--- Projects/Q-2/stringUtils.js
+++ Projects/Q-2/stringUtils.js
@@ -6,7 +6,7 @@ function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-function reverseString(str) {
+function reverseString(str){
     return str.split("").reverse().join("");
 }
 
