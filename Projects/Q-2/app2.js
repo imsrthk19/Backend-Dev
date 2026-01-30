48d6cc9abe1a6c39e8ab240d3f75165daad73608
Sarthak Srivastava <145271363+imsrthk19@users.noreply.github.com>
2026-01-30 22:00:44 +0530

    Delete Projects directory

--- Projects/Q-2/app2.js
+++
@@ -1,5 +0,0 @@
-const stringUtils = require("./stringUtils");
-const text = "nodejs";
-console.log("Capitalized String:", stringUtils.capitalize(text));
-console.log("Reversed String:", stringUtils.reverseString(text));
-console.log("Total Vowel Count:", stringUtils.countVowels(text));
